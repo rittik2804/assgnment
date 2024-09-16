0363643a57bb08d743733a6b1458c553b51bb85a
rittik2804 <rittik2804@gmail.com>
2024-09-17 00:32:41 +0530

    change page name
    17.09.2024

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_application_1/app/feature/Home/binding/home_page_binding.dart';
 import 'package:flutter_application_1/app/feature/Home/view/homeScreen.dart';
-import 'package:flutter_application_1/app/feature/Home/view/secondPage.dart';
 import 'package:flutter_application_1/app/routes/app_pages.dart';
 import 'package:get/get.dart';
+
+import '../feature/Home/view/secondScreen.dart';
 
 class AppRoutes {
   static String getHome() => AppPages.home;
